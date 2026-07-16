95058de78ffbed48de0ccbd10952b07aa5c7510e
Kenneth Sonoda <114664532+Kenny-still12@users.noreply.github.com>
2026-07-16 22:18:26 +1000

    feat: add SVG icons for various platforms and social media
    - Added icons for Bluesky, Discord, Documentation, GitHub, Social, and X in `public/icons.svg`.
    - Included a React logo and Vite logo in the assets directory.
    - Introduced a new CSS file `App.css` for styling components.
    - Created main application structure in `App.jsx` with a simple greeting.
    - Set up Tailwind CSS in `index.css` for utility-first styling.
    - Configured Vite with React and Tailwind CSS in `vite.config.js`.
    - Established the main entry point in `main.jsx` to render the application.

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <>
+      <div className="w-1/2 h-24 bg-green-800 flex justify-center items-center mt-10 m-auto rounded-lg shadow">
+        <h1 className="text-5xl m-4 italic text-orange-500 flex justify-center items-center">Hello</h1>
+      </div>
+    </>
+
+  )
+}
