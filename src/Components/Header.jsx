5eda956896e6b3899bd7b6902fe6c5fcb44b7a82
Kenneth Sonoda <114664532+Kenny-still12@users.noreply.github.com>
2026-08-08 20:20:17 +1000

    added weather description

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <header className="w-screen flex justify-between items-center px-2 py-2.5">
+        <header className="flex w-full justify-between items-center py-2.5">
             <h1 className="text-2xl text-sky-500 tracking-widest italic font-medium">Weathers<span className="text-black not-italic">4</span>You</h1>
             <p className="text-lg">date</p>
         </header>
