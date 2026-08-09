429acc5bf7e4b4cf4e95c19d3d367aa4efbf1afd
Kenneth Sonoda <114664532+Kenny-still12@users.noreply.github.com>
2026-08-09 18:56:19 +1000

    Created local variable to simplify the syntax

--- src/Components/WeatherCard.jsx
+++ src/Components/WeatherCard.jsx
@@ -1,11 +1,13 @@
 export default function WeatherCard({ weatherResult }) {
+    const data = weatherResult.data;
+
     return (
         <div className="flex flex-col gap-3 items-center">
-            <h1 className="text-center text-5xl text-gray-100">{weatherResult.data.name}, {weatherResult.data.sys?.country}</h1>
-            <img src={`https://openweathermap.org/img/wn/${weatherResult.data.weather[0].icon}@2x.png`} alt="" className="w-40" />
-            <h2 className="text-center text-3xl text-gray-200">{weatherResult.data.weather[0].description}</h2>
-            <h2 className="text-center text-6xl text-gray-100">{weatherResult.data.main?.temp} °C</h2>
-            <p className="text-center text-xl text-gray-300">Feels like {weatherResult.data.main?.feels_like} °C</p>
+            <h1 className="text-center text-5xl text-gray-100">{data.name}, {data.sys?.country}</h1>
+            <img src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} alt="" className="w-40" />
+            <h2 className="text-center text-3xl text-gray-200">{data.weather[0].description}</h2>
+            <h2 className="text-center text-6xl text-gray-100">{data.main?.temp} °C</h2>
+            <p className="text-center text-xl text-gray-300">Feels like {data.main?.feels_like} °C</p>
         </div>
     )
 }
